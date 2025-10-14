ca9c911d8635f6e69ccb13de074ef00586d69685
wh3248 <user.name>
2025-10-14 13:36:23 -0400

    add nlc_prompts.txt file

--- scripts/in_container/build_nlc.sh
+++ scripts/in_container/build_nlc.sh
@@ -2,6 +2,6 @@ echo Running inside container
 echo NLC_DIR $NLC_DIR
 #cp /usr/lib64/gfortran/modules/netcdf*.mod $LIS_MODESMF
 echo "CONFIGURE"
-bash ./configure.sh
+bash ./configure.sh < nlc_prompts.txt
 echo BUILD
 bash ./build.sh --clean
